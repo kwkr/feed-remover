604e7a8f2f92c7aa7a521826fe7c9db6adc200a1
kwkr <kawka.maciej.93@gmail.com>
2023-10-20 22:32:43 +0200

    add domain check

--- content.js
+++ content.js
@@ -1,20 +1,26 @@
+let domain = window.location.host;
+
 // Facebook
-let id = setInterval(()=>{
-    const feed = document.querySelector('[role="feed"]');
-    if(feed) {
-        feed.remove();
-        clearInterval(id);
-        console.log('removed feed');
-    }
-}, 100);
+if (domain.includes('facebook')) {
+    let id = setInterval(() => {
+        const feed = document.querySelector('[role="feed"]');
+        if (feed) {
+            feed.remove();
+            clearInterval(id);
+            console.log('removed feed');
+        }
+    }, 100);
+}
 
 
 // YouTube
-let id1 = setInterval(()=>{
-    const feed = document.getElementById('contents');
-    if(feed) {
-        feed.remove();
-        clearInterval(id1);
-        console.log('removed feed');
-    }
-}, 100);
+if (domain.includes('youtube')) {
+    let id1 = setInterval(() => {
+        const feed = document.getElementById('contents');
+        if (feed) {
+            feed.remove();
+            clearInterval(id1);
+            console.log('removed feed');
+        }
+    }, 100);
+}
